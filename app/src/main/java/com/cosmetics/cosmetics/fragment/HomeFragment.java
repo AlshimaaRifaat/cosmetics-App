2233988dc17a989f5e678c785f95d8c77d77fbd7
Alshimaa <alshimaa.rifaat@gmail.com>
2019-07-03 11:38:02 +0200

    done cart fragment

--- app/src/main/java/com/cosmetics/cosmetics/fragment/HomeFragment.java
+++ app/src/main/java/com/cosmetics/cosmetics/fragment/HomeFragment.java
@@ -6,14 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 
 import com.cosmetics.cosmetics.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+    @BindView(R.id.icon_cart)
+    ImageView iconCart;
+    Unbinder unbinder;
 
+    View view;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -24,7 +34,16 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view= inflater.inflate(R.layout.fragment_home, container, false);
+        unbinder= ButterKnife.bind(this,view);
+        iconCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.relative_home,new CartFragment())
+                        .addToBackStack(null).commit();
+            }
+        });
+        return view;
     }
 
 }
