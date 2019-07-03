b9512d94f264e089d8a19b28969cd23fa4a63423
Alshimaa <alshimaa.rifaat@gmail.com>
2019-07-03 13:15:06 +0200

    more frag done

--- app/src/main/java/com/cosmetics/cosmetics/fragment/DetailsMyOrdersFragment.java
+++ app/src/main/java/com/cosmetics/cosmetics/fragment/DetailsMyOrdersFragment.java
@@ -6,14 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.cosmetics.cosmetics.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DetailsMyOrdersFragment extends Fragment {
+//T_rate_product1
 
+    @BindView(R.id.T_rate_product1)
+    TextView rateProduct1Txt;
+    Unbinder unbinder;
 View view;
     public DetailsMyOrdersFragment() {
         // Required empty public constructor
@@ -25,6 +35,15 @@ View view;
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_details_my_orders, container, false);
+
+        unbinder= ButterKnife.bind(this,view);
+        rateProduct1Txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.relative_details,new RateMyOrdersFragment())
+                        .addToBackStack(null).commit();
+            }
+        });
         return view;
     }
 
