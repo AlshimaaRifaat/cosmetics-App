812c5cb344b10a09b6d26005583487a5a787a9e9
Alshimaa <alshimaa.rifaat@gmail.com>
2019-07-03 14:06:03 +0200

    more frag done

--- app/src/main/java/com/cosmetics/cosmetics/fragment/MoreFragment.java
+++ app/src/main/java/com/cosmetics/cosmetics/fragment/MoreFragment.java
@@ -6,25 +6,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.cosmetics.cosmetics.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MoreFragment extends Fragment {
-
+    @BindView(R.id.T_favorite)
+    TextView favoriteTxt;
+    Unbinder unbinder;
 
     public MoreFragment() {
         // Required empty public constructor
     }
 
-
+View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
-    }
+        view= inflater.inflate(R.layout.fragment_more, container, false);
 
+        unbinder= ButterKnife.bind(this,view);
+        favoriteTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.relative_more,new FavoriteFragment())
+                        .addToBackStack(null).commit();
+            }
+        });
+        return view;
+    }
 }
