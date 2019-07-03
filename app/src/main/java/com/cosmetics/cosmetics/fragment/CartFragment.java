cb95234c834c3b6098436dc8b2296fc42ebd56b4
Alshimaa <alshimaa.rifaat@gmail.com>
2019-07-03 11:40:46 +0200

    done cart fragment

---
+++ app/src/main/java/com/cosmetics/cosmetics/fragment/CartFragment.java
@@ -0,0 +1,36 @@
+package com.cosmetics.cosmetics.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+
+import com.cosmetics.cosmetics.R;
+
+import butterknife.BindView;
+import butterknife.Unbinder;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CartFragment extends Fragment {
+
+
+View view;
+    public CartFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view= inflater.inflate(R.layout.fragment_cart, container, false);
+        return view;
+    }
+
+}
