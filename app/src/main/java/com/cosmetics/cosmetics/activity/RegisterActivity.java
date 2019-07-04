46a0c58e816f8b25e2fc1cfd24baf20f8f0d2a7d
Alshimaa <alshimaa.rifaat@gmail.com>
2019-07-04 13:15:10 +0200

    app design done

---
+++ app/src/main/java/com/cosmetics/cosmetics/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.cosmetics.cosmetics.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cosmetics.cosmetics.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
