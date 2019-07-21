3e362554559e7ec3df1d79cab8895493ab3cd7ea
Alshimaa <alshimaa.rifaat@gmail.com>
2019-07-21 11:25:10 +0200

    app design done+2 btn (cat & brand)

--- app/src/main/java/com/cosmetics/cosmetics/activity/LoginActivity.java
+++ app/src/main/java/com/cosmetics/cosmetics/activity/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.cosmetics.cosmetics.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,13 +14,14 @@ import com.cosmetics.cosmetics.R;
 public class LoginActivity extends AppCompatActivity {
 Button loginBtn;
 Button signUpBtn;
-TextView signUpTxt;
+TextView signUpTxt,forgotPasswordTxt;
+Typeface customFontRegular;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         init();
-
+        //setFont();
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,10 +36,17 @@ TextView signUpTxt;
                 startActivity(intent);
             }
         });
+
     }
+
+   /* private void setFont() {
+        customFontRegular = Typeface.createFromAsset( getApplicationContext().getAssets(), "robotoFont/Roboto-Regular.ttf" );
+        forgotPasswordTxt.setTypeface( customFontRegular );
+    }*/
 
     private void init() {
         loginBtn=findViewById(R.id.btn_login);
         signUpTxt=findViewById(R.id.T_sign_up);
+        forgotPasswordTxt=findViewById(R.id.T_forgot_password);
     }
 }
--- app/src/main/java/com/cosmetics/cosmetics/fragment/ProductsFragment.java
+++ app/src/main/java/com/cosmetics/cosmetics/fragment/ProductsFragment.java
@@ -1,30 +1,72 @@
 package com.cosmetics.cosmetics.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.cosmetics.cosmetics.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProductsFragment extends Fragment {
-
+    @BindView(R.id.btn_category) Button btnCategory;
+    @BindView(R.id.btn_brands)Button btnBrands;
+    private Unbinder unbinder;
 
     public ProductsFragment() {
         // Required empty public constructor
     }
 
-
+View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_products, container, false);
+        view= inflater.inflate(R.layout.fragment_products, container, false);
+        unbinder= ButterKnife.bind(this,view);
+        chooseType();
+        return view;
+    }
+
+    private void chooseType() {
+        btnBrands.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                btnBrands.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.btn_pink));
+                btnBrands.setTextColor(Color.rgb(240, 245, 251));
+                btnCategory.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.btn_white));
+                btnCategory.setTextColor(Color.rgb(	113, 113, 113)	);
+                /*TripStatus();
+                clickAddTrip();*/
+
+            }
+        });
+        btnCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                btnCategory.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.btn_pink));
+                btnCategory.setTextColor(Color.rgb(240, 245, 251));
+                btnBrands.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.btn_white));
+                btnBrands.setTextColor(Color.rgb(  	113, 113, 113));
+                /*TripStatusMgdwla();
+                clickAddTrip();*/
+
+                // reservation_presenter.reservation(String.valueOf(PAGE),Clintid,Lang,Type);
+            }
+        });
     }
 
 }
