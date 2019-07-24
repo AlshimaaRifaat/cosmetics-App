e96a210f066f1cb634ed2bb0df94fb1302eed7c9
Alshimaa <alshimaa.rifaat@gmail.com>
2019-07-24 16:19:30 +0200

    details latest prod done

---
+++ app/src/main/java/com/cosmetics/cosmetics/view/DetailsHomeLatestProductsView.java
@@ -0,0 +1,9 @@
+package com.cosmetics.cosmetics.view;
+
+import com.cosmetics.cosmetics.model.LatestProductsData;
+
+import java.util.List;
+
+public interface DetailsHomeLatestProductsView {
+    void showDetailsHomeLatestProducts(LatestProductsData latestProductsData);
+}
