2645d6eaf3337f96c8b08db30a6d27016f7c0703
Alshimaa <alshimaa.rifaat@gmail.com>
2019-07-28 14:00:20 +0200

    +details product category done

---
+++ app/src/main/java/com/cosmetics/cosmetics/view/DetailsProductCategoryView.java
@@ -0,0 +1,7 @@
+package com.cosmetics.cosmetics.view;
+
+import com.cosmetics.cosmetics.model.ProductCategoryData;
+
+public interface DetailsProductCategoryView {
+    void showDetailsProductCategory(ProductCategoryData productCategoryData);
+}
