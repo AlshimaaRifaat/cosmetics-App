ae5b0f107965152af5edb3633ab4bca6622f0640
Alshimaa <alshimaa.rifaat@gmail.com>
2019-08-04 12:19:19 +0200

    add cart &(prod color done-1) >>check network con >>show msg email already used

---
+++ app/src/main/java/com/cosmetics/cosmetics/view/OnClickProductColorView.java
@@ -0,0 +1,7 @@
+package com.cosmetics.cosmetics.view;
+
+import com.cosmetics.cosmetics.model.DetailsProductColorsData;
+
+public interface OnClickProductColorView {
+    void showOnClickProductColorResult(DetailsProductColorsData detailsProductColorsData);
+}
