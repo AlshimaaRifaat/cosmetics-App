28fca69d31243869b6dc9a1e2858dec8769eddf6
Alshimaa <alshimaa.rifaat@gmail.com>
2019-08-05 11:21:26 +0200

    min cart done-1 >>check network con-1 >>show msg email already used >> get details prod from home feature

---
+++ app/src/main/java/com/cosmetics/cosmetics/view/MinQuantityView.java
@@ -0,0 +1,7 @@
+package com.cosmetics.cosmetics.view;
+
+import com.cosmetics.cosmetics.model.GetListCartData;
+
+public interface MinQuantityView {
+    void showMinQuantityCart(GetListCartData getListCartData);
+}
