3c88758006c7a0be22d92a29be53cf4db4cb5657
Alshimaa <alshimaa.rifaat@gmail.com>
2019-08-05 13:44:44 +0200

    min cart done+(delete cart-1)done >>check network con-1 >>show msg email already used >> get details prod from home feature

---
+++ app/src/main/java/com/cosmetics/cosmetics/view/DeleteItemCartView.java
@@ -0,0 +1,7 @@
+package com.cosmetics.cosmetics.view;
+
+import com.cosmetics.cosmetics.model.GetListCartData;
+
+public interface DeleteItemCartView {
+    void showDeleteItemCartView(GetListCartData getListCartData,int Position);
+}
