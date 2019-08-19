fe788785f818aa9cb7534b0203f3c5f4d6b9ef8b
Alshimaa <alshimaa.rifaat@gmail.com>
2019-08-19 13:34:40 +0200

    notifications nums done order now done api done-(street) create rate api done - (desss) >> get details prod from featuree prod

---
+++ app/src/main/java/com/cosmetics/cosmetics/view/NotificationNumsView.java
@@ -0,0 +1,9 @@
+package com.cosmetics.cosmetics.view;
+
+import com.cosmetics.cosmetics.model.GetListCartData;
+
+import java.util.List;
+
+public interface NotificationNumsView {
+    void showNotificationNums(List<GetListCartData> listCartDataList);
+}
